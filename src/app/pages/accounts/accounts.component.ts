d7829f30684efc64c217a0aae80eef5ebd0aa605
Ruben Cardenas <rcardenas@newgistics.com>
2018-12-18 21:33:13 -0600

    Added accounts. Still need to group them

--- src/app/pages/accounts/accounts.component.ts
+++ src/app/pages/accounts/accounts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/services/account.service';
+import { Account, AccountType } from 'src/app/interfaces';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-accounts',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts.component.scss']
 })
 export class AccountsComponent implements OnInit {
+  accounts: Account[];
+  accountsChunk: any[];
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
+    this.accountService.getAllAccounts().subscribe((accounts: Account[]) => {
+      this.accounts = accounts;
+      this.accountsChunk = _.chain(this.accounts)
+      .groupBy((account: Account) => account.type)
+      .map((value: Account[], key: string) => {
+
+        return {
+          title: this.getAccountTypeName(value[0].type),
+          entries: value,
+          totalValue: _.sumBy(value, v => v.currentBalance)
+        };
+      })
+      .value();
+    });
+  }
+
+  getAccountTypeName(type): string {
+    switch (type) {
+      case AccountType.Debit:
+        return "Debito";
+      case AccountType.Cash:
+        return "Efectivo";
+      case AccountType.Credit:
+        return "Credito";
+      case AccountType.Savings:
+        return "Ahorros Ruben";
+      case AccountType.SarahiSavings:
+        return "Ahorros Sarahi";
+    }
+    return "";
   }
 
 }
